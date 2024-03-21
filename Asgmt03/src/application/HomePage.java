98e97a74b186dd09c3fc34b3b6f6eb5002fa0a05
EricSJSU-DataScience <fredkiki1986@gmail.com>
2024-03-21 16:55:05 -0700

    update_20240321_1654

--- Asgmt03/src/application/HomePage.java
+++ Asgmt03/src/application/HomePage.java
@@ -22,47 +22,14 @@ public class HomePage extends Application{
 		VBox menuNavigation = new VBox(30);
 		menuNavigation.setStyle("-fx-background-color: rgba(72, 155, 105, 0.7); -fx-padding: 30;");
 		
-		//Navigation Buttons
-		Button button_NewCategory = new Button("New Category");
-		Button button_NewLocation = new Button("New Location");
-		Button button_NewAsset = new Button("   New Asset   ");
-		Button button_Search = new Button("     Search      ");
-		Button button_Reports = new Button(/*"     Reports     " */ "");
+		StackPane button_NewCategory = navigationButton("New Category");
+		StackPane button_NewLocation = navigationButton("New Location");
+		StackPane button_NewAsset = navigationButton("New Asset");
+		StackPane button_Search = navigationButton("Search");
+		StackPane button_Reports = navigationButton("Reports");
 		
-		//modify button
-		button_NewCategory.setStyle("-fx-background-color: #0068B8; " + 
-									"-fx-background-radius: 20; " + "-fx-background-insets: 2;" +
-									"-fx-text-fill: Black; " + 
-									"-fx-font-size: 20px; " + 
-									"-fx-padding: 12 18 12 18");
-		button_NewLocation.setStyle("-fx-background-color: #0068B8; " + 
-									"-fx-background-radius: 20; " + "-fx-background-insets: 2;" +
-									"-fx-text-fill: Black; " + 
-									"-fx-font-size: 20px; " + 
-									"-fx-padding: 12 18 12 18");
-		button_NewAsset.setStyle("-fx-background-color: #0068B8; " + 
-								 "-fx-background-radius: 20; " + "-fx-background-insets: 2;" +
-								 "-fx-text-fill: Black; " + 
-								 "-fx-font-size: 20px; " + 
-								 "-fx-padding: 12 18 12 18");
-		button_Search.setStyle("-fx-background-color: #0068B8; " + 
-						   	   "-fx-background-radius: 20; " + "-fx-background-insets: 2;" +
-							   "-fx-text-fill: Black; " + 
-							   "-fx-font-size: 20px; " + 
-							"-fx-padding: 12 18 12 18");
+		menuNavigation.getChildren().addAll(button_NewCategory, button_NewLocation, button_NewAsset, button_Search, button_Reports);
 		
-		button_Reports.setStyle("-fx-background-color: #0068B8;" + 
-								"-fx-background-radius: 20; " + "-fx-background-insets: 2;" +
-								"-fx-text-fill: Black; " + 
-								"-fx-font-size: 20px; " + 
-								"-fx-padding: 12 120 12 40");
-		Text label_Reports = new Text("Reports Info");
-		label_Reports.setFont(Font.font("Arial", 18));
-		
-		StackPane button5 = new StackPane(button_Reports, label_Reports);
-		
-		
-		menuNavigation.getChildren().addAll(button_NewCategory, button_NewLocation, button_NewAsset, button_Search, /*button_Reports*/ button5);
 		
 		
 		// Welcome message + app name
@@ -81,25 +48,15 @@ public class HomePage extends Application{
 		content2.getChildren().addAll(appName);
 		
 		//image
-		/*
-		ImageView box = new ImageView(new Image("art_box.jpg"));
-		
-		HBox content3 = new HBox(10);
-		content3.getChildren().addAll(box);
-		*/
+
 		
 		
 		// 
 		HBox mainBkgd = new HBox(menuNavigation, content1, content2);
-		//mainBkgd.setSpacing(10);
-		//mainBkgd.setAlignment(Pos.CENTER);
-		//
+
 		Scene scene = new Scene(mainBkgd, 800, 600);
 		
-		//
-		//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-		//String css = this.getClass().getResource("application.css").toExternalForm();
-		//scene.getStylesheets().add(css);
+
 		
 		
 		homeStage.setTitle("SortYourLife");
@@ -111,6 +68,26 @@ public class HomePage extends Application{
 		
 		
 	}
+	
+	public StackPane navigationButton(String arg) {
+		StackPane res;
+		//create button 
+		Button button = new Button();
+		button.setStyle("-fx-background-color: #0068B8;" + 
+				"-fx-background-radius: 20; " + "-fx-background-insets: 2;" +
+				"-fx-text-fill: Black; " + 
+				"-fx-font-size: 20px; " + 
+				"-fx-padding: 12 120 12 40");
+		//create text
+		Text label = new Text(arg);
+		label.setFont(Font.font("Arial", 18));
+		//
+		res = new StackPane(button, label);
+		
+		return res;
+	}
+	
+	
 	
 	public static void main(String[] args) {
 		launch(args);
