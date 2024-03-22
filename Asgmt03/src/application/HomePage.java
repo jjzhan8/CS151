7ff437882954b35af9054a583202b0f7396f367b
EricSJSU-DataScience <fredkiki1986@gmail.com>
2024-03-21 18:18:40 -0700

    update_20230321_1818

--- Asgmt03/src/application/HomePage.java
+++ Asgmt03/src/application/HomePage.java
@@ -22,11 +22,13 @@ public class HomePage extends Application{
 		VBox menuNavigation = new VBox(30);
 		menuNavigation.setStyle("-fx-background-color: rgba(72, 155, 105, 0.7); -fx-padding: 30;");
 		
-		StackPane button_NewCategory = navigationButton("New Category");
-		StackPane button_NewLocation = navigationButton("New Location");
-		StackPane button_NewAsset = navigationButton("New Asset");
-		StackPane button_Search = navigationButton("Search");
-		StackPane button_Reports = navigationButton("Reports");
+		Button button_NewCategory = navigationButton("New Category");
+		Button button_NewLocation = navigationButton("New Location");
+		Button button_NewAsset = navigationButton("New Asset");
+		Button button_Search = navigationButton("Search");
+		Button button_Reports = navigationButton("Reports");
+		
+
 		
 		menuNavigation.getChildren().addAll(button_NewCategory, button_NewLocation, button_NewAsset, button_Search, button_Reports);
 		
@@ -48,15 +50,25 @@ public class HomePage extends Application{
 		content2.getChildren().addAll(appName);
 		
 		//image
-
+		/*
+		ImageView box = new ImageView(new Image("art_box.jpg"));
+		
+		HBox content3 = new HBox(10);
+		content3.getChildren().addAll(box);
+		*/
 		
 		
 		// 
 		HBox mainBkgd = new HBox(menuNavigation, content1, content2);
-
+		//mainBkgd.setSpacing(10);
+		//mainBkgd.setAlignment(Pos.CENTER);
+		//
 		Scene scene = new Scene(mainBkgd, 800, 600);
 		
-
+		//
+		//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		//String css = this.getClass().getResource("application.css").toExternalForm();
+		//scene.getStylesheets().add(css);
 		
 		
 		homeStage.setTitle("SortYourLife");
@@ -69,27 +81,41 @@ public class HomePage extends Application{
 		
 	}
 	
-	public StackPane navigationButton(String arg) {
-		StackPane res;
-		//create button 
-		Button button = new Button();
-		button.setStyle("-fx-background-color: #0068B8;" + 
-				"-fx-background-radius: 20; " + "-fx-background-insets: 2;" +
-				"-fx-text-fill: Black; " + 
-				"-fx-font-size: 20px; " + 
-				"-fx-padding: 12 120 12 40");
-		//create text
-		Text label = new Text(arg);
-		label.setFont(Font.font("Arial", 18));
-		//
-		res = new StackPane(button, label);
+
+	
+	public Button navigationButton(String arg) {
+		Button res = new Button(arg);
+		
+		res.setStyle("-fx-background-color: #0068B8;" + 
+					"-fx-background-radius: 25; " + "-fx-background-insets: 2;" +
+					"-fx-text-fill: Black; " + 
+					"-fx-font-size: 20px; ");
+		
+		res.setMinSize(180, 50);
 		
 		return res;
 	}
-	
 	
 	
 	public static void main(String[] args) {
 		launch(args);
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
